d1a02e74ef7d6fc1b5a6ad3a80a399c9dc268b1c
Adam Kawa <kawaa@spotify.com>
2014-04-18 16:34:57 +0200

    Simpler local config

---
+++ src/test/java/com/spotify/beetest/TestCaseTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spotify.beetest;
+
+import java.io.IOException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kawaa
+ */
+public class QueryGeneratorTest {
+
+    String resourcesDir = "src/main/resources";
+
+    @Test
+    public void test1() throws IOException, Exception {
+        String query = new TestCase().getSetupQuery(
+                "src/main/resources/tests/setup1.hql");
+    }
+
+    @Test
+    public void test2() throws IOException, Exception {
+        String query = new TestCase().getTestedQuery("output",
+                "src/main/resources/tests/query1.hql");
+    }
+
+    @Test
+    public void test3() throws IOException, Exception {
+        String expected = "DROP TABLE IF EXISTS words;\n"
+                + "CREATE TABLE words(word STRING, length INT)\n"
+                + "ROW FORMAT DELIMITED FIELDS TERMINATED BY '\\t'\n"
+                + "STORED AS TEXTFILE;\n"
+                + "LOAD DATA LOCAL INPATH 'src/main/resources/tests/input1.txt' OVERWRITE INTO TABLE words;\n"
+                + "INSERT OVERWRITE LOCAL DIRECTORY '/tmp/beetest' \n"
+                + "SELECT MAX(length) FROM words;";
+        
+        String[] args = {
+            "-e", "src/main/resources/tests/output1.txt",
+            "-s", "src/main/resources/tests/setup1.hql",
+            "-q", "src/main/resources/tests/query1.hql",
+            "-o", "/tmp/beetest",};
+        String query = new TestCase().run(args);
+        // assertEquals(expected, query);
+    }
+}
